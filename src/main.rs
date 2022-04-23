d3002a7d2c8b7154b91403c88ce59a7b1f3f462b
Masashi Adachi <masashi.adachi@gmail.com>
2022-04-23 12:24:48 +0900

    add struct Pet

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
 const DB_PATH: &str = "./data/db.json";
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
+}
 
 fn main() {
     println!("Hello, world!");
